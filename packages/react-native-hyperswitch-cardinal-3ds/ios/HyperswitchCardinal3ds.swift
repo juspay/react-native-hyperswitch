d78b63b7d940eaea4aed883844fef3af30ec5c1b
ChiragKV-Juspay <chirag.kv@juspay.in>
2025-09-25 16:02:39 +0530

    Revert "init cardinal"
    This reverts commit 8595f7bd3695def6408c3437501da2cbe24d9918.

--- packages/react-native-hyperswitch-cardinal-3ds/ios/HyperswitchCardinal3ds.swift
+++
@@ -1,8 +0,0 @@
-@objc(HyperswitchCardinal3ds)
-class HyperswitchCardinal3ds: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
-}
